68e668f6d51ff789cd4e7a2d651e5f96543782d4
DioTepes <nathan.livemedia@gmail.com>
2022-08-23 00:03:09 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//--- D20 ---//
+function rollD20() {
+    let d20Result = document.getElementById("d20Result");
+    let d20 = Math.floor(Math.random()*20+1);
+      d20Result.innerHTML = d20;
+    };
+    
+    //--- D12 ---//
+    function rollD12() {
+    let d12Result = document.getElementById("d12Result");
+    let d12 = Math.floor(Math.random()*12+1);
+      d12Result.innerHTML = d12;
+    };
+    
+    //--- D10 ---//
+    function rollD10() {
+    let d10Result = document.getElementById("d10Result");
+    let d10 = Math.floor(Math.random()*10+1);
+      d10Result.innerHTML = d10;
+    };
+    
+    //--- D8 ---//
+    function rollD8() {
+    let d8Result = document.getElementById("d8Result");
+    let d8 = Math.floor(Math.random()*8+1);
+      d8Result.innerHTML = d8;
+    };
+    
+    //--- D6 ---//
+    function rollD6() {
+    let d6Result = document.getElementById("d6Result");
+    let d6 = Math.floor(Math.random()*6+1);
+      d6Result.innerHTML = d6;
+    };
+    
+    //--- D6 ---//
+    function rollD4() {
+    let d4Result = document.getElementById("d4Result");
+    let d4 = Math.floor(Math.random()*4+1);
+      d4Result.innerHTML = d4;
+    };
+    
